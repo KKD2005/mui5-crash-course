88142a6d5717acd3e0f42d24cdeaa499950420fd
Konnie Duan <kddypublic84@yahoo.com>
2024-06-21 01:16:13 -0700

    Custom Theme with Providers Complete

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -4,51 +4,78 @@ import Typography from '@mui/material/Typography';
 import Box from "@mui/material/Box"
 import {AccessTime} from "@mui/icons-material"
 import Rating from '@mui/material/Rating';
+import {createTheme, ThemeProvider} from "@mui/material";
+const theme = createTheme({
+    components:{
+        MuiTypography:{
+            variants:[
+                {
+                    props:{
+                        variant:"body2"
+                    },
+                    style:{
+                        fontSize:11
+                    }
+                },
+                {
+                    props:{
+                        variant:"body3"
+                    },
+                    style:{
+                        fontSize:9
+                    }
+                }
+            ]
+        }
+    }
+})
 
 const TourCard = ()=>{
     return( 
     <Grid item xs = {3}>
-        <Paper elevation={3}>
-            <img className="img" src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg" alt=""></img>
-            
-            <Box paddingX={1}>
-                <Typography variant="subtitle1" component="h2">
-                    Immerse into the Falls
-                    
-                </Typography>
-                <Box
-                    sx={{
-                        display:"flex",
-                        alignItems:"center"
-                    }}
-                >
-                    <AccessTime sx={{width: 12.5}}></AccessTime>
-                    <Typography variant="body2" component="p" marginLeft={0.5}>
-                        5 hours
+        <ThemeProvider theme={theme}>
+            <Paper elevation={3}>
+                <img className="img" src="https://tcproduction.blob.core.windows.net/media/%7B240f8b72-1159-4fd3-a150-0a837f50ba4a%7D.2573758641_297d6d19fa_o.jpg" alt=""></img>
+                
+                <Box paddingX={1}>
+                    <Typography variant="subtitle1" component="h2">
+                        Immerse into the Falls
+                        
                     </Typography>
-                </Box>
-                <Box
-                    sx={{
-                        display:"flex",
-                        alignItems:"center"
-                    }}
-                    marginTop={3}>
-                        <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+                    <Box
+                        sx={{
+                            display:"flex",
+                            alignItems:"center"
+                        }}
+                    >
+                        <AccessTime sx={{width: 12.5}}></AccessTime>
                         <Typography variant="body2" component="p" marginLeft={0.5}>
-                            4.5
+                            5 hours
                         </Typography>
-                        <Typography variant="body3" component="p" marginLeft={1.5}>
-                            (655 reviews)
+                    </Box>
+                    <Box
+                        sx={{
+                            display:"flex",
+                            alignItems:"center"
+                        }}
+                        marginTop={3}>
+                            <Rating name="read-only" value={4.5} readOnly precision={0.5} size="small"/>
+                            <Typography variant="body2" component="p" marginLeft={0.5}>
+                                4.5
+                            </Typography>
+                            <Typography variant="body3" component="p" marginLeft={1.5}>
+                                (655 reviews)
+                            </Typography>
+                    </Box>
+                    <Box>
+                        <Typography variant="h6" component="h3" marginTop={0}>
+                            From C $147
                         </Typography>
+                    </Box>
                 </Box>
-                <Box>
-                    <Typography variant="h6" component="h3" marginTop={0}>
-                        From C $147
-                    </Typography>
-                </Box>
-            </Box>
-            
-        </Paper>
+                
+            </Paper>
+        </ThemeProvider>
     </Grid>
     );
 }
